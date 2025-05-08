131e8f7f73c5154109e51098cae09b7c0540ae1e
RlmUwU <jerome@guilleminet.fr>
2025-05-08 16:21:45 +0200

    Cleanup of the code

--- TunicERTrackerAdapter.py
+++ TunicERTrackerAdapter.py
@@ -1,9 +1,9 @@
 import csv
-import tkinter as tk
-from operator import index
 from tkinter import filedialog
 
 if __name__ == '__main__':
+
+    print("Choose the Plugin ER tracker file")
     ap_er_tracker = filedialog.askopenfilename(
         title="Select AP ER tracker file",
         filetypes=[("CSV Files", "*.csv")]
@@ -23,10 +23,6 @@ if __name__ == '__main__':
             names[line["Archipelago"]] = line["Scipio"]
             names_list.append(line["Archipelago"])
 
-    print(ap_entrances)
-    print(names)
-    print(names_list)
-
     worlds = {}
     prev_world = (None,1)
     for i,world in enumerate(names):
@@ -37,28 +33,27 @@ if __name__ == '__main__':
             worlds[prev_world[0]][1] = i-1
         prev_world= (world,i)
     worlds[prev_world[0]][1] = i
-    print(worlds)
+
+    print("Writing into output.csv")
 
     with open("output.csv", mode='w', newline='', encoding='utf-8') as file:
         file.write("#tunic\n")
         for entrance in ap_entrances:
             if entrance["To"] == "":
                 continue
-            try:
-                entrance_translation = names[entrance["From"]]
-                destination_translation = names[entrance["To"]]
+            entrance_translation = names[entrance["From"]]
+            destination_translation = names[entrance["To"]]
 
-                entrance_index = names_list.index(entrance["From"])
-                destination_index = names_list.index(entrance["To"])
-                for world_range in worlds:
-                    if worlds[world_range][0] <= entrance_index <= worlds[world_range][1]:
-                        entrance_world = world_range
-                    if worlds[world_range][0] <= destination_index <= worlds[world_range][1]:
-                        destination_world = world_range
-                file.write(f"tunic,{entrance_world},{entrance_translation},warp,{destination_world},{destination_translation},\n")
-            except:
-                pass
+            entrance_index = names_list.index(entrance["From"])
+            destination_index = names_list.index(entrance["To"])
+            for world_range in worlds:
+                if worlds[world_range][0] <= entrance_index <= worlds[world_range][1]:
+                    entrance_world = world_range
+                if worlds[world_range][0] <= destination_index <= worlds[world_range][1]:
+                    destination_world = world_range
+            file.write(f"tunic,{entrance_world},{entrance_translation},warp,{destination_world},{destination_translation},\n")
 
+    print("File written successfully")
 
 
 
